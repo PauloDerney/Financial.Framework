45f00a022992309b7f1a748172332a8401bf5f6b
Paulo Barros <5by5.barros@voeazul.com.br>
2021-02-09 00:08:22 -0300

    add loggeduser in command base

--- src/Financial.Framework.Domain/Entities/Command.cs
+++ src/Financial.Framework.Domain/Entities/Command.cs
@@ -5,5 +5,7 @@ namespace Financial.Framework.Domain.Entities
     public class Command<T> : IRequest<T>
     {
         public string CorrelationId { get; set; }
+
+        public string LoggedUser { get; set; }
     }
 }
