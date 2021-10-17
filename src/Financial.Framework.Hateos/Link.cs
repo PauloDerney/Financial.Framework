c678248bad348b89f46a997ddc41252600bbff2f
Paulo Eduardo Derney de Barros <5by5.barros@voeazul.com.br>
2021-10-17 11:49:54 -0300

    Add hateos project

---
+++ src/Financial.Framework.Hateos/Link.cs
@@ -0,0 +1,33 @@
+﻿namespace Financial.Framework.Hateos
+{
+    public struct Link
+    {
+        public Link(string href, string rel, MethodType method)
+        {
+            Href = href;
+            Rel = rel;
+            Method = method.ToString();
+        }
+
+        public string Href { get; set; }
+
+        public string Rel { get; set; }
+
+        public string Method { get; set; }
+
+        public static Link CreateLinkToGet(string baseHref, string href, string rel)
+            => new Link($"{baseHref}/{href}", rel, MethodType.Get);
+
+        public static Link CreateLinkToPost(string baseHref, string href, string rel)
+            => new Link($"{baseHref}/{href}", rel, MethodType.Post);
+
+        public static Link CreateLinkToPatch(string baseHref, string href, string rel)
+            => new Link($"{baseHref}/{href}", rel, MethodType.Patch);
+
+        public static Link CreateLinkToPut(string baseHref, string href, string rel)
+            => new Link($"{baseHref}/{href}", rel, MethodType.Put);
+
+        public static Link CreateLinkToDelete(string baseHref, string href, string rel)
+            => new Link($"{baseHref}/{href}", rel, MethodType.Delete);
+    }
+}
---
+++ src/Financial.Framework.Hateos/MethodType.cs
@@ -0,0 +1,11 @@
+﻿namespace Financial.Framework.Hateos
+{
+    public enum MethodType
+    {
+        Get,
+        Post,
+        Patch,
+        Delete,
+        Put
+    }
+}
